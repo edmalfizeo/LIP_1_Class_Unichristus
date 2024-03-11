2f611862d0d2a6d4e66b542042a5e32f88e7e7c8
edmalfizeo <edmalfizeo@gmail.com>
2024-03-11 19:26:07 -0300

    Fixing Exercise8

--- ExercisesHome2/Exercise8.java
+++ ExercisesHome2/Exercise8.java
@@ -1,20 +1,17 @@
 package ExercisesHome2;
 
-import java.util.Scanner;
-
 public class Exercise8 {
-    public static double iterativeDivide(double... args) {
+    public static double iterativeDivide(double... args){
         double total = 1;
 
         try {
             for (double i : args) {
                 if (i == 0.0) {
-                    System.out.print("There is a number that is 0 so the program ");
                     throw new ArithmeticException("Cannot divide by zero.");
                 }
                 total /= i;
             }
-        } catch (Exception x) {
+        } catch (ArithmeticException x) {
             System.out.println(x.getMessage());
         }
         return total;
@@ -25,6 +22,7 @@ public class Exercise8 {
         try {
             newPhrase = phrase.toLowerCase();
         }catch(Exception x) {
+            System.out.println("\n");
             System.out.println(x.getMessage());
         }
         return newPhrase;
@@ -32,12 +30,7 @@ public class Exercise8 {
 
     public static void main(String[] args) {
         System.out.printf("The total of the iterativeDivide without zero is: %f", iterativeDivide(1, 2.1, 3.2, 4.1, 0));
-        try{
-            System.out.printf("\nThe phrase in lower case is: %s", toLowerCase("OLA A TODOS"));
-        }catch(NullPointerException x) {
-            System.out.println(x.getMessage());
-            System.out.println("An error occurs!!");
-        }
+        System.out.printf("\nThe phrase in lower case is: %s", toLowerCase("OLA A TODOS"));
     }
 }
 
